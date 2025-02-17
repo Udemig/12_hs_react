95ec9eea490cec56751d89fbd664ce7d7dfd23b3
Furkan Evin <furkanevin00@gmail.com>
2025-02-18 02:23:52 +0300

    update

--- 5_unit-test/4_ice-cream/README.md
+++ 5_unit-test/4_ice-cream/README.md
@@ -10,3 +10,16 @@
 - redux-mock-store
 - react-testing-library
 - jest
+
+## (TDD) Test Driven Development
+
+- Önce testler yazılır ardından (component/function) yazılır.
+- red to green test
+
+## (BDD) Behaviour Driven Development
+
+- Önce (component/function) yazılır arından testleri yapılır.
+
+## 100% Test Covarage
+
+- Bütün dinamik özelliklerin test edildiği seneryo
--- 5_unit-test/4_ice-cream/src/redux/cartSlice.js
+++ 5_unit-test/4_ice-cream/src/redux/cartSlice.js
@@ -4,9 +4,35 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: { cart: [] },
   reducers: {
-    addToCart: (state, { payload }) => {},
+    addToCart: (state, { payload }) => {
+      // sepette aynı üründen var mı
+      const found = state.cart.find((item) => item.id === payload.item.id && item.type === payload.selectedType);
 
-    deleteFromCart: (state, { payload }) => {},
+      if (found) {
+        // eğer sepette aynı elemandan varsa miktarını arttır
+        found.amount++;
+      } else {
+        // eğer sepette aynı elemandan yoksa sepete ekle
+        state.cart.push({
+          ...payload.item,
+          type: payload.selectedType,
+          amount: 1,
+        });
+      }
+    },
+
+    deleteFromCart: (state, { payload }) => {
+      // sepette aynı üründen var mı
+      const index = state.cart.findIndex((item) => item.id === payload.id && item.type === payload.type);
+
+      if (state.cart[index].amount > 1) {
+        // eğer miktar 1'den fazla ise miktarı azalt
+        state.cart[index].amount--;
+      } else {
+        // eğer miktar 1 ise ise ürünü kaldır
+        state.cart.splice(index, 1);
+      }
+    },
 
     creteOrder: (state, { payload }) => {},
   },
